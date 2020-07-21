8ac19df55a53c626cd59fdc8a41cf0809f84d9e2
Kevin Tran <kevintran@Kevins-MBP.hsd1.ca.comcast.net>
2020-07-20 17:46:15 -0700

    game and user database

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-const express = require('express')
-const APP = express();
-const PORT = 3000
+const express = require('express');
 
-const mongoose = require('mongoose')
-// dbname changed to game-data
-const mongoDB = 'mongodb+srv://test1:test1@game-data.xi8ae.mongodb.net/game-date?retryWrites=true&w=majority'
-mongoose.connect(mongoDB, { useNewUrlParser: true })
-const db = mongoose.connection
+// express app
+const app = express();
 
-db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+// mongoose
+const mongoose = require('mongoose');
 
-APP.get('/', (req, res) => {
-    res.send('Hello World!')
+// connecting to game models
+const gameSchema = require('./models/gameSchema');
+
+// connecting to user models
+const userSchema = require('./models/userSchema');
+
+// connects to mongodb 
+const dbURI = 'mongodb+srv://KevinTran:Testing123@game-data.xi8ae.mongodb.net/game-dataretryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true}, function(err, db){});
+
+// default connection
+const db = mongoose.connection;
+
+// bind connection to error even
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+const handleError = function() {
+    console.error(err);
+}
+
+const newGame = new gameSchema({title: 'Legend of Zelda', description: 'blah blah'});
+
+newGame.save(function (err) {
+    if(err) return handleError(err);
+    console.log('hopefully it worked');
+});
+
+const newUser = new userSchema({userName: 'betaTester1', password: 'cheating'});
+
+newUser.save(function(err) {
+    if(err) return handleError(err);
+    console.log('hopefully it worked');
 })
-
-APP.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
