43364bc66d54999ae2ac8b8f1bdde58b75fe8270
JABermudez11 <justinanbermudez@gmail.com>
2020-08-29 21:27:09 -0700

    index finally works

--- routes/games.js
+++ routes/games.js
@@ -3,30 +3,32 @@ const router = express.Router();
 const Game = require('../models/game')
 
 // index
-router.get('/', async(req, res) => {
-    console.log("games get request hit!")
-    res.send("get request succesful")
-    // console.log(req.headers);
+router.get('/', async(req, res) => {    
+    // res.send("get request succesful")    
 
-    const game = await Game.find()
+    // const game = await Game.find()
 
-    if (game) {
-        console.log(game)
-        // res.json(game)
-    } else {
-        res.redirect('/')
+    try {
+        const games = await Game.find()
+        res.json(games)
+    } catch (err) {
+        res.send('Error: ' + err)
     }
     
     // try{
-    //     const games = await Game.find()
-    //     res.json(games)
+    //     // const games = await Game.find()
+    //     const {title, description, genre } = req.query
+    //     // if (games) {
+    //     //     console.log("games found")
+    //     // }
+    //     // res.json(games)
     // }catch(err){
     //     res.send('error ' + err)
     // }
 })
 
 router.post('/', async (req, res) => {
-    const game = new Game({
+    const game = new Game({        
         title: req.body.title,
         description: req.body.description,
         genre: req.body.genre,
@@ -39,7 +41,13 @@ router.post('/', async (req, res) => {
     
     try{
         const recentGame = await game.save()
-        res.json(recentGame)
+        if (recentGame){
+            res.json(recentGame)
+            console.log("game saved")
+        } else {
+            console.log("game not saved")
+        }
+        
     }catch(err){
         res.send('Error '+ err)
     }
