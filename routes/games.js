552b61ad42151190853044f931394a4b5974986b
JABermudez11 <justinanbermudez@gmail.com>
2020-09-05 04:57:11 -0700

    put request start

--- routes/games.js
+++ routes/games.js
@@ -4,27 +4,13 @@ const Game = require('../models/game')
 
 // index
 router.get('/', async(req, res) => {    
-    // res.send("get request succesful")    
-
-    // const game = await Game.find()
-
+   
     try {
         const games = await Game.find()
         res.json(games)
     } catch (err) {
         res.send('Error: ' + err)
-    }
-    
-    // try{
-    //     // const games = await Game.find()
-    //     const {title, description, genre } = req.query
-    //     // if (games) {
-    //     //     console.log("games found")
-    //     // }
-    //     // res.json(games)
-    // }catch(err){
-    //     res.send('error ' + err)
-    // }
+    }   
 })
 
 router.post('/', async (req, res) => {
@@ -34,7 +20,7 @@ router.post('/', async (req, res) => {
         genre: req.body.genre,
         release_date: req.body.release_date
     })
-    // game = req.body
+    
     console.log("post request sent to games")
     console.log(game)
     console.log(game.title)
@@ -52,6 +38,10 @@ router.post('/', async (req, res) => {
         res.send('Error '+ err)
     }
 
+})
+
+router.put('/:gameId', async (req, res) => {
+    
 })
 
 module.exports = router
