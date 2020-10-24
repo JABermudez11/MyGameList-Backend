748862e5cef23364f5b9d08ab03aaebe40f2f5cb
JABermudez11 <justinanbermudez@gmail.com>
2020-10-23 23:59:16 -0700

    FRONTEND TO BACKEND WORKS

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config()
 
+app.use(cors());
 app.use(bodyParser.json()); 
 
 const PORT = process.env.PORT || 4000
